2a73f7917e71a5d72dca1a308bf8c2246c80603b
Nick Kralevich <nnk@google.com>
2015-06-02 16:00:21 -0700

    idol3: remove persist.sys.usb.config override
    The variable persist.sys.usb.config is no longer honored by the
    framework,
    and it's presence causes unnecessary toggling of the USB driver, which
    disonnects ADB and makes the device's connection unstable.
    
    Delete it.
    
    Bug: 21404762
    Bug: 18905620
    Change-Id: I78267c6c003f4c1fe0f58df7e4c4df748025080f

--- device.mk
+++ device.mk
@@ -56,9 +56,6 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 TARGET_BOOTANIMATION_HALF_RES := true
 
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    persist.sys.usb.config=mtp
-
 $(call inherit-product, frameworks/native/build/phone-hdpi-2048-dalvik-heap.mk)
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 
