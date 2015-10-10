77c98a6f5532c183e8dba92a1cb63a5a4cfa01b8
c457 <android.c357@gmail.com>
2015-10-10 16:12:22 -0500

    idol3: use stock wlan module
    * We don't have the macaddr modification that alcatel made. For
    this reason use the pre-compiled module. Alcatel has already provided a
    check to skip signature check of wlan module.

--- device.mk
+++ device.mk
@@ -235,4 +235,5 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wcnss/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wcnss/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wcnss/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin \
+    $(LOCAL_PATH)/wcnss/wlan.ko:system/lib/modules/wlan.ko \
     $(LOCAL_PATH)/wcnss/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
