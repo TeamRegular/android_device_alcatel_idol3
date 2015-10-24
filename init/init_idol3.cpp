05d7f369be4c2c2c2a1044baecfa49c8dd4b30c1
Bruno Martins <bgcngm@gmail.com>
2015-10-24 13:33:39 +0100

    idol3: Update libinit and fix compile errors
    Change-Id: Id5607806889ae1b0add506fae314cab67b2cec2e

--- init/init_idol3.cpp
+++ init/init_idol3.cpp
@@ -40,12 +40,12 @@ void common_properties()
         property_set("ro.build.product", "idol3");
 }
 
-void dualsim_properties(char multisim_config[])
+void dualsim_properties(char const multisim_config[])
 {
     property_set("persist.radio.multisim.config", multisim_config);
 }
 
-void gsm_properties(char default_network[])
+void gsm_properties(char const default_network[])
 {
     property_set("ro.telephony.default_network", default_network);
 }
