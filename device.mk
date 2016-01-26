c09c23141313fcb6435accaad7edf193bc0b0aeb
c457 <android.c357@gmail.com>
2016-01-26 09:44:31 -0600

    idol3: switch to init.qcom.power and update devfreqs

--- device.mk
+++ device.mk
@@ -202,7 +202,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.qcom.bt.sh \
-    init.qcom.post_boot.sh \
     init.qcom.power.rc \
     init.qcom.rc \
     init.qcom.usb.rc \
