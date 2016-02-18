44ff39095a2e6c67bef406d66a842868e9e67fc7
c457 <android.c357@gmail.com>
2016-02-18 04:29:09 -0600

    idol3: remove unneeded qcom usb script

--- device.mk
+++ device.mk
@@ -202,7 +202,6 @@ PRODUCT_PACKAGES += \
     init.qcom.power.rc \
     init.qcom.rc \
     init.qcom.usb.rc \
-    init.qcom.usb.sh \
     init.recovery.qcom.rc \
     ueventd.qcom.rc
 
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -41,14 +41,6 @@ LOCAL_SRC_FILES    := etc/init.qcom.usb.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.usb.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.usb.sh
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
 # Copy the power config for recovery too
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.recovery.qcom.rc
