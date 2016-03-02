3791f92faae653de7650f99f43063235889bca36
c457 <android.c357@gmail.com>
2016-03-01 18:14:38 -0600

    idol3: import opensource gps LA.BR.1.1.3_rb1.46

--- device.mk
+++ device.mk
@@ -132,6 +132,9 @@ PRODUCT_PACKAGES += \
     qcom.fmradio
 
 # GPS
+PRODUCT_PACKAGES += \
+    gps.msm8916
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
     $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
