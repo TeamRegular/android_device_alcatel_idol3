03a1d9757ff1b3ff663e9c88dac50d6411d39180
c457 <android.c357@gmail.com>
2016-09-20 09:05:23 -0500

    idol3: Use opensource kernel
    * About time Alcatel

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,6 +1,4 @@
-#BOARD_DTBTOOL_ARGS := -2
-
-BOARD_CUSTOM_BOOTIMG_MK := device/alcatel/idol3/mkbootimg.mk
+BOARD_DTBTOOL_ARGS := -2
 
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.bootloader=L7VAB0B0BG00 androidboot.selinux=permissive
@@ -11,13 +9,13 @@ BOARD_RAMDISK_OFFSET     := 0x01000000
 
 #ENABLE_CPUSETS := true
 
-TARGET_KERNEL_SOURCE := kernel/qcom/msm8916
+TARGET_KERNEL_SOURCE := kernel/alcatel/msm8916
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_USES_UNCOMPRESSED_KERNEL := true
 
 # Kernel
-TARGET_KERNEL_CONFIG := idol3_defconfig
-TARGET_PREBUILT_KERNEL := device/alcatel/idol3/kernel
+TARGET_KERNEL_CONFIG := cyanogenmod_idol3_defconfig
+
 
