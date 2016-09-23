85be208fa9029cb334983916dd8be1dfe79016b2
c457 <android.c357@gmail.com>
2016-09-23 13:02:08 -0500

    idol3: Add missing audio configs and blobs
    Change-Id: I58572465b53141792c4f0a1796152d0753c4ddc9

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -25,7 +25,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/tfa9897/seltech_top.cnt:system/etc/tfa9897/seltech_top.cnt \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/audio/mixer_paths_mtp.xml:system/etc/mixer_paths_mtp.xml
+    $(LOCAL_PATH)/audio/mixer_paths_mtp.xml:system/etc/mixer_paths_mtp.xml \
+    $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
+    $(LOCAL_PATH)/audio/sound_trigger_mixer_paths_wcd9306.xml:system/etc/sound_trigger_mixer_paths_wcd9306.xml \
+    $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml
 
 # Permissions
 #PRODUCT_COPY_FILES += \
