LineageOS/android_device_zte_axon7

amp: Handle voice routing
* Also handle mono left/right vs speaker

Change-Id: I2fcf160bf495246287899bf453b65eaa711bb371
Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

audio_amplifier/audio_amplifier.c
@@ -45,31 +45,52 @@ typedef struct tfa9890_device {
 
 static tfa9890_device_t *tfa9890_dev = NULL;
 
-static int is_spkr_needed(uint32_t snd_device) {
-    int spkr_needed = 0;
+enum {
+    NO_SPEAKER = 0,
+    MONO_RIGHT,
+    MONO_LEFT,
+    STEREO_SPEAKER
+};
 
+static int is_spkr_needed(uint32_t snd_device) {
     switch (snd_device) {
         case SND_DEVICE_OUT_SPEAKER:
         case SND_DEVICE_OUT_VOICE_SPEAKER:
-            spkr_needed = 1;
-            break;
+        case SND_DEVICE_OUT_HANDSET:
+        case SND_DEVICE_OUT_VOICE_HANDSET:
+            return MONO_RIGHT;
+        default:
+            return NO_SPEAKER;
     }
-
-    return spkr_needed;
 }
 
 static int amp_enable_output_devices(hw_device_t *device, uint32_t devices, bool enable) {
     tfa9890_device_t *tfa9890 = (tfa9890_device_t*) device;
 
-    if (is_spkr_needed(devices)) {
-        if (enable) {
-            tfa9890->speaker_needed(1, 1);
-            tfa9890->speaker_on();
-            } else {
-               tfa9890->speaker_needed(0, 0);
-               tfa9890->speaker_off();
-            }
+    if (enable) {
+        switch (is_spkr_needed(devices)) {
+            case STEREO_SPEAKER:
+                tfa9890->speaker_needed(1, 1);
+                tfa9890->speaker_on();
+                break;
+            case MONO_RIGHT:
+                tfa9890->speaker_needed(0, 1);
+                tfa9890->speaker_on();
+                break;
+            case MONO_LEFT:
+                tfa9890->speaker_needed(1, 0);
+                tfa9890->speaker_on();
+                break;
+            case NO_SPEAKER:
+                tfa9890->speaker_needed(0, 0);
+                tfa9890->speaker_off();
+                break;
+        }
+    } else {
+        tfa9890->speaker_needed(0, 0);
+        tfa9890->speaker_off() ;
     }
+
     return 0;
 }
 

TeamRegular/android_device_alcatel_idol3

audio_amplifier/audio_amplifier.c
@@ -39,39 +39,54 @@ typedef struct tfa9897_device {
     amplifier_device_t amp_dev;
     void *lib_ptr;
     int (*init)(int);
-    int (*enable)(int, int);
-    int (*disable)(int);
+    int (*speaker_on)(int, int);
+    int (*speaker_off)(int);
 } tfa9897_device_t;
 
 static tfa9897_device_t *tfa9897_dev = NULL;
 
 #define SAMPLE_RATE 48000
 
-static int is_speaker(uint32_t snd_device) {
-    int speaker = 0;
+enum {
+    NO_SPEAKER = 0,
+    MONO_RIGHT,
+    STEREO_SPEAKER
+};
 
+static int is_spkr_needed(uint32_t snd_device) {
     switch (snd_device) {
         case SND_DEVICE_OUT_SPEAKER:
         case SND_DEVICE_OUT_SPEAKER_REVERSE:
         case SND_DEVICE_OUT_SPEAKER_AND_HEADPHONES:
         case SND_DEVICE_OUT_VOICE_SPEAKER:
-            speaker = 1;
-            break;
+            return STEREO_SPEAKER;
+        case SND_DEVICE_OUT_HANDSET:
+        case SND_DEVICE_OUT_VOICE_HANDSET:
+            return MONO_RIGHT;
+        default:
+            return NO_SPEAKER;
     }
-
-    return speaker;
 }
 
 static int amp_enable_output_devices(hw_device_t *device, uint32_t devices, bool enable, bool disable) {
     tfa9897_device_t *tfa9897 = (tfa9897_device_t*) device;
 
-    if (is_speaker(devices)) {
-        if (enable) {
-            tfa9897->enable(0, 2);
-        } else {
-            tfa9897->disable(1);
+    if (enable) {
+        switch (is_spkr_needed(devices)) {
+            case STEREO_SPEAKER:
+                tfa9897->speaker_on(0, 2);
+                break;
+            case MONO_RIGHT:
+                tfa9897->speaker_on();
+                break;
+            case NO_SPEAKER:
+                tfa9897->speaker_off(1);
+                break;
         }
+    } else {
+        tfa9897->speaker_off(1) ;
     }
+
     return 0;
 }
 
@@ -125,8 +140,8 @@ static int amp_module_open(const hw_module_t *module,
     }
 
     *(void **)&tfa9897_dev->init = dlsym(tfa9897_dev->lib_ptr, "tfa9897_init");
-    *(void **)&tfa9897_dev->enable = dlsym(tfa9897_dev->lib_ptr, "tfa9897_SpeakerOn");
-    *(void **)&tfa9897_dev->disable = dlsym(tfa9897_dev->lib_ptr, "tfa9897_SpeakerOff");
+    *(void **)&tfa9897_dev->speaker_on = dlsym(tfa9897_dev->lib_ptr, "tfa9897_SpeakerOn");
+    *(void **)&tfa9897_dev->speaker_off = dlsym(tfa9897_dev->lib_ptr, "tfa9897_SpeakerOff");
 
     if (!tfa9897_dev->init || !tfa9897_dev->enable || !tfa9897_dev->disable) {
         ALOGE("%s:%d: Unable to find required symbols", __func__, __LINE__);